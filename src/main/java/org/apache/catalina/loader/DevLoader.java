e98c49c6aa21498c8133677029a4ccde8a99dca0
tadashi <tadashi@192.168.11.3>
2025-07-24 01:16:06 +0900

    change class path directories to WebResources

--- src/main/java/org/apache/catalina/loader/DevLoader.java
+++ src/main/java/org/apache/catalina/loader/DevLoader.java
@@ -6,14 +6,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
-import java.net.URI;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 import org.apache.catalina.Globals;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.WebResourceRoot.ResourceSetType;
 
 import jakarta.servlet.ServletContext;
 
@@ -23,7 +27,7 @@ import jakarta.servlet.ServletContext;
  */
 public final class DevLoader extends WebappLoader {
 	/** Information */
-	public static final String INFO = "org.apache.catalina.loader.DevLoader/1.0";
+	public static final String INFO = "org.apache.catalina.loader.DevLoader/11.0";
 
 	/** クラスパスファイル */
 	private static final String WEB_CLASS_PATH_FILE = ".#webclasspath";
@@ -31,11 +35,26 @@ public final class DevLoader extends WebappLoader {
 	private static final String TOMCAT_PLUGIN_FILE = ".tomcatplugin";
 
 	/**
-	 * @see org.apache.catalina.util.LifecycleBase#startInternal()
+	 * {@inheritDoc}
 	 */
 	@Override
 	protected void startInternal() throws LifecycleException {
 		log("Starting DevLoader");
+
+		final WebResourceRoot resourceRoot = getContext().getResources();
+		resourceRoot.setCachingAllowed(false);
+
+		final List<File> entries = readWebClassPathEntries().stream().map(File::new).filter(File::exists).toList();
+
+		for (final File f : entries) {
+			if (f.isDirectory()) {
+				final var url = toURL(f);
+				if (url != null) {
+					resourceRoot.createWebResourceSet(ResourceSetType.CLASSES_JAR, "/WEB-INF/classes", url, "/");
+					log("added " + f.toURI());
+				}
+			}
+		}
 
 		super.startInternal();
 
@@ -45,43 +64,47 @@ public final class DevLoader extends WebappLoader {
 			return;
 		}
 
-		final StringBuilder classpath = new StringBuilder();
-		for (final String entry : readWebClassPathEntries()) {
-			File f = new File(entry);
-			if (f.exists()) {
-				if (f.isDirectory() && !entry.endsWith("/")) {
-					f = new File(entry + "/");
+		for (final File f : entries) {
+			if (!f.isDirectory()) {
+				final var url = toURL(f);
+				if (url != null) {
+					webappClassLoader.addURL(url);
+					log("added " + f.toURI());
 				}
-				final URI uri = f.toURI();
-				try {
-					webappClassLoader.addURL(uri.toURL());
-				} catch (final MalformedURLException e) {
-					logError(e.getMessage());
-					continue;
-				}
-				classpath.append(f.toString()).append(File.pathSeparator);
-				log("added " + uri.toString());
-			} else {
-				logError(entry + " does not exist !");
 			}
 		}
 
-		final String cp = (String) getServletContext().getAttribute(Globals.CLASS_PATH_ATTR);
-		if (cp != null) {
-			for (final String token : cp.split(File.pathSeparator)) {
-				// only on windows
-				if (token.charAt(0) == '/' && token.charAt(2) == ':') {
-					classpath.append(token.substring(1));
-				} else {
-					classpath.append(token);
-				}
-				classpath.append(File.pathSeparator);
-			}
+		final String classpath = entries.stream().map(File::toString).collect(Collectors.joining(File.pathSeparator));
+		if (!classpath.isEmpty()) {
+			final var cp = classpath + getClassPathAttribute();
+			getServletContext().setAttribute(Globals.CLASS_PATH_ATTR, cp);
+			log("JSPCompiler Classpath = " + cp);
 		}
+	}
 
-		getServletContext().setAttribute(Globals.CLASS_PATH_ATTR, classpath.toString());
+	/**
+	 * クラスパス属性文字列取得
+	 *
+	 * @return クラスパス属性文字列
+	 */
+	private String getClassPathAttribute() {
+		final String cp = Objects.toString(getServletContext().getAttribute(Globals.CLASS_PATH_ATTR), "");
+		return cp.isEmpty() ? "" : File.pathSeparator + cp;
+	}
 
-		log("JSPCompiler Classpath = " + classpath);
+	/**
+	 * URL変換
+	 *
+	 * @param f File
+	 * @return URL
+	 */
+	private URL toURL(final File f) {
+		try {
+			return f.toURI().toURL();
+		} catch (final MalformedURLException e) {
+			logError(e.getMessage());
+			return null;
+		}
 	}
 
 	/**
@@ -109,13 +132,15 @@ public final class DevLoader extends WebappLoader {
 	 */
 	protected List<String> readWebClassPathEntries() {
 		final File prjDir = getProjectRootDir();
-		if (prjDir == null) {
-			return Collections.emptyList();
+		if (prjDir != null) {
+			log("projectdir=" + prjDir.getAbsolutePath());
+
+			final File cpFile = new File(prjDir, WEB_CLASS_PATH_FILE);
+			if (cpFile.exists()) {
+				return loadWebClassPathFile(cpFile);
+			}
 		}
-
-		log("projectdir=" + prjDir.getAbsolutePath());
-
-		return loadWebClassPathFile(prjDir);
+		return Collections.emptyList();
 	}
 
 	/**
@@ -138,25 +163,20 @@ public final class DevLoader extends WebappLoader {
 	/**
 	 * Webクラスパスファイル読込
 	 *
-	 * @param prjDir プロジェクトディレクトリ
+	 * @param cpFile クラスパスファイル
 	 * @return 読込行リスト
 	 */
-	protected List<String> loadWebClassPathFile(final File prjDir) {
-		final File cpFile = new File(prjDir, WEB_CLASS_PATH_FILE);
-		if (!cpFile.exists()) {
-			return Collections.emptyList();
-		}
-
+	protected List<String> loadWebClassPathFile(final File cpFile) {
 		final List<String> rc = new ArrayList<>();
-		try (FileInputStream fis = new FileInputStream(cpFile)) {
-			try (InputStreamReader reader = new InputStreamReader(fis, StandardCharsets.UTF_8)) {
-				try (BufferedReader lr = new BufferedReader(reader)) {
-					String line = null;
-					while ((line = lr.readLine()) != null) {
-						line = line.replace('\\', '/');
-						rc.add(line);
-					}
-				}
+		try (
+			FileInputStream fis = new FileInputStream(cpFile);
+			InputStreamReader reader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+			BufferedReader lr = new BufferedReader(reader);
+		) {
+			String line = null;
+			while ((line = lr.readLine()) != null) {
+				line = line.replace('\\', '/');
+				rc.add(line);
 			}
 		} catch (final IOException ex) {
 			logError(ex.getMessage());
